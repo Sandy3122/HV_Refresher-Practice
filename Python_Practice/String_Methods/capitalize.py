14cd9a4ab4ce4ebcb399394921ea63dc48c4658f
Sandeep <seeram87@gmail.com>
2022-12-22 01:02:53 +0530

    Some Problems On Nested Loops And Hacker Problems

--- Python_Practice/String_Methods/capitalize.py
+++ Python_Practice/String_Methods/capitalize.py
@@ -1,6 +1,6 @@
 #Capitalize() Method
 #Every Starting Letter Will Starts With Capital
 
-name = "Sandeep"
+name = input("Enter A String : ")
 capitalize_name = name.capitalize()
 print(capitalize_name)
