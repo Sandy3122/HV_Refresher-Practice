08dd97661c10040812a91dc84758e5afcd21e66e
Sandeep <seeram87@gmail.com>
2023-01-11 16:15:04 +0530

    Mysql With Python Has Added

--- SQL/SQL Assessments/Sql Assement_3/Q5.sql
+++ SQL/SQL Assessments/Sql Assement_3/Q5.sql
@@ -6,5 +6,5 @@ have not placed any orders or if they have placed orders through their sales man
 
 SELECT c.consumer_name, s.manager_city, o.order_no, o.order_date, o.purchase_amount, s.manager_name
 FROM consumer c
-LEFT JOIN orders o ON c.consumer_id = o.consumer_id
-LEFT JOIN sales_manager s ON o.sales_manager_id = s.sales_manager_id;
+INNER JOIN orders o ON c.consumer_id = o.consumer_id
+INNER JOIN sales_manager s ON o.sales_manager_id = s.sales_manager_id;
