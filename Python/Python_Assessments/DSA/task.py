f657ebfdc4ce78fc6b1aff61921532a165807513
Sandeep <seeram87@gmail.com>
2023-02-21 22:58:24 +0530

    DSA Assessment

--- Python/Python_Assessments/DSA/task.py
+++ Python/Python_Assessments/DSA/task.py
@@ -1,11 +1,5 @@
-'''
-Recursive Function - A function that calls itself is known as a recursive function. Declare a function - “dup_ele()” 
-which will find the frequency of duplicate elements from the array and display it individually.
-We need to count the duplicate elements in the array. This function - “dup_ele()” is a recursive function that will call itself in the code. 
-'''
+# Write a program (W.A.P) to implement the sorting technique to find the frequency of the duplicate elements in the array.
 
+def dup_elments():
+     
 
-def dup_ele(arr){
-    
-
-}
